af91e3a14960946bfe489332ddf8de4e76460578
daupet <daupet@example.com>
2015-11-15 22:36:58 +0900

    制御プログラムのソースファイルを追加した
    加速度センサから車体の傾き角度を推定して，モータに電圧を出力する。
    計算処理は主に整数型で行う。その方が計算が速いらしい。
    
    ・ ロボットの車体角度を取得する関数をつくった。
    ・ モータへ電圧を出力する関数をつくった。未完成である。
    
    TODO
    ・ motor_output() 関数の中身をつくる。ロボットのモータドライバを確認しなが
       ら行う必要がありそうだ。
    ・ 制御器を実装する。とりあえず今は比例制御のみとする。主な作業は比例ゲイン
       の調整となるだろう。

---
+++ inverted_pendulum.ino
@@ -0,0 +1,77 @@
+// 加速度センサからの入力ピン
+const int pin_ax = 3;
+const int pin_ay = 4;
+const int pin_az = 5;
+
+// 左右モータへの出力ピン
+const int pin_lmot = 8;
+const int pin_rmot = 9;
+
+/**
+ * 現在の車体の傾きを取得する。
+ * 角度の表現は，加速度センサのx軸を0radとして，
+ * y軸回りの方向を正方向とする。
+ *
+ * return: 現在の車体の傾き角度 [10^-3 rad]
+ */
+int get_angle(void);
+
+/**
+ * モータへの出力を設定する。
+ *
+ *
+ * output: モータへの電圧出力 [mV]
+ */
+void motor_output(int output);
+
+
+void setup()
+{
+}
+
+void loop()
+{
+  int angle;   // 車体の傾き [10^-3 rad]
+  int dev;       // 目標値と現在の傾きとの偏差 [10^-3 rad]
+  int output;  // モータに入力する電圧 [mV]
+
+  int Kp = 1.0;         // 比例ゲイン
+  int angle_cmd = 00;  // 車体傾きの目標値 [10^-3 rad]
+
+  // 車体の傾きを取得する
+  angle = get_angle();
+
+  // 車体の傾きの偏差（目標値と現在値の差）を求め，それをゲイン倍してモータ出力を計算する
+  
+
+  // モータにトルクを入力する
+  motor_output(output);
+}
+
+
+int get_angle(void)
+{
+  // 今回使う加速度センサは電源電圧3.3Vの場合，
+  // 加速度0で1.65Vを出力するようになっている。
+  // Arduinoでは0～5Vを0～1023で取得するので，
+  // 1.65V÷5V×1023＝337.59 ―×10→ 3375 となる
+  static const int offset = 3375;
+
+  // センサから加速度を取得する。
+  int ax = analogRead(pin_ax) * 10 - offset;
+  int ay = analogRead(pin_ay) * 10 - offset;
+  int az = analogRead(pin_az) * 10 - offset;
+
+  // x軸とz軸の加速度から偏角を求める。
+  // 計算にはarctanの近似式を用いた：[http://blogs.yahoo.co.jp/fermiumbay2/38729661.html]より
+  int tmp = (-az*10)/ax;
+  return (12*tmp*tmp+4500)/(27*tmp*tmp+4500)*tmp*100; // [10^-3 rad]
+}
+
+
+void motor_output(int output)
+{
+  analogWrite(pin_lmot, output);
+  analogWrite(pin_rmot, output);
+}
+
